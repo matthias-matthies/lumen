7b4ae447bd1b8c13f678d45c9903bce3952b5747
matthias-matthies <github@matthias-matthies.com>
2024-09-27 10:49:46 +0200

    initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
+import 'primeicons/primeicons.css'
 import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -18,7 +21,13 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
             .use(ZiggyVue)
+            // .component()
             .mount(el);
     },
     progress: {
